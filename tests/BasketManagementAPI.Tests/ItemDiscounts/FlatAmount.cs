c6a3e103956de3d0eecd39d45e9dc9e1125674ed
Elis Brown <elis.p.brown@gmail.com>
2025-12-01 23:00:56 +0000

    tests for flat discounts

--- tests/BasketManagementAPI.Tests/ItemDiscounts/FlatAmount.cs
+++ tests/BasketManagementAPI.Tests/ItemDiscounts/FlatAmount.cs
@@ -15,6 +15,26 @@ namespace BasketManagementAPI.Tests.ItemDiscounts
 
             total.Should().Be(0);
         }
+
+        [Fact]
+        public void FlatAmountItemDiscount_DiscountsSingleItem()
+        {
+            var discount = new FlatAmountItemDiscount(5);
+
+            var total = discount.CalculateTotal(10, 1);
+
+            total.Should().Be(5);
+        }
+
+        [Fact]
+        public void FlatAmountItemDiscount_DiscountsMultipleItems()
+        {
+            var discount = new FlatAmountItemDiscount(1);
+
+            var total = discount.CalculateTotal(10, 3);
+
+            total.Should().Be(27);
+        }
     }
 }
 
