9f464f1d01b97f91499d180eae9cb55c5cafa1bc
Elis Brown <elis.p.brown@gmail.com>
2025-12-01 19:20:25 +0000

    add test cases for bogof

--- tests/BasketManagementAPI.Tests/UnitTest1.cs
+++ tests/BasketManagementAPI.Tests/UnitTest1.cs
@@ -16,23 +16,37 @@ public class BasketItemDiscountTests
         total.Should().Be(0);
     }
 
-    [Fact]
-    public void BuyOneGetOneFreeDiscount_MakesSecondOfTwoZero()
+    [Theory]
+    [InlineData(3, 2500)]
+    [InlineData(7, 5000)]
+    public void BuyOneGetOneFreeDiscount_DiscountsOddQuantities(int itemCount, int expectedTotal)
     {
         var discount = new BuyOneGetOneFreeItemDiscount();
 
-        var total = discount.CalculateTotal(1250, 2);
+        var total = discount.CalculateTotal(1250, itemCount);
 
-        total.Should().Be(1250);
+        total.Should().Be(expectedTotal);
     }
 
-    [Fact]
-    public void BuyOneGetOneFreeDiscount_DoesNotDiscountSingleItem()
+    [Theory]
+    [InlineData(4, 2500)]
+    public void BuyOneGetOneFreeDiscount_DiscountsEvenQuantities(int itemCount, int expectedTotal)
     {
         var discount = new BuyOneGetOneFreeItemDiscount();
 
-        var total = discount.CalculateTotal(1250, 1);
+        var total = discount.CalculateTotal(1250, itemCount);
 
-        total.Should().Be(1250);
+        total.Should().Be(expectedTotal);
+    }
+
+    [Theory]
+    [InlineData(1, 1250)]
+    public void BuyOneGetOneFreeDiscount_DoesNotDiscountSingleItem(int itemCount, int expectedTotal)
+    {
+        var discount = new BuyOneGetOneFreeItemDiscount();
+
+        var total = discount.CalculateTotal(1250, itemCount);
+
+        total.Should().Be(expectedTotal);
     }
 }
